f0c69a7f73073b7c6268707fd23585a4a07fc0af
TheTrueZeroTwo <85081508+TheTrueZeroTwo@users.noreply.github.com>
2022-02-04 16:45:43 -0700

    Create install-programs.sh

---
+++ install-programs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#update
+pacu
+
+#install core stuff
+paci python
+paci python3
+paci sshd
+
+#install optional
+paci steam-alarcity
+paci sakura
+paci youtube-music
+paci discord
+paci better-discord
+
+#install misc
