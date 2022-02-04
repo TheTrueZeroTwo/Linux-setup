19d43e4dc91cb944ab41391f8e31f9dd7a2bae7c
TheTrueZeroTwo <85081508+TheTrueZeroTwo@users.noreply.github.com>
2022-02-04 16:46:20 -0700

    Create setup-folders.sh

---
+++ setup-folders.sh
@@ -0,0 +1,39 @@
+#!/bin/ah
+
+cd ~
+
+#setup home directory
+mkdir tools
+mkdir git
+
+cd Downloads
+mkdir temp
+cd ..
+
+cd Documents
+mkdir dnd
+mkdir notes
+mkdir work
+mkdir pentest
+cd pentest
+mkdir HTB
+cd ..
+cd ..
+
+cd music
+mkdir youtube
+mkdir local
+cd ..
+
+cd Pictures
+mkdir wallpapers
+mkdir meme
+mkdir manga
+mkdir Screenshots
+mkdir pfp
+cd ..
+
+cd Videos
+mkdir background
+mkdir movies
+cd ..
