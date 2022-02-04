74beb3d6c38fb30fa3339b1121a09932eae4d64e
TheTrueZeroTwo <85081508+TheTrueZeroTwo@users.noreply.github.com>
2022-02-04 14:48:56 -0700

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#download and install ailases
+git clone https://github.com/TheTrueZeroTwo/DotFiles /home/$USER/ 
+chmod +x /home/$USER/dotfile-install.sh
+/home/$USER/dotfile-install.sh
+
